4626ddab1a4273bc42b4eb36f6591c43399957d2
lulu <s987337613@gmail.com>
2017-06-18 15:56:31 +0800

    create project and write first unit test

---
+++ ShoppingCar/ShoppingCar/DiscountCalcualtor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCar
+{
+    public class DiscountCalcualtor
+    {
+        public decimal CalcualtePayoutMoney(int bookCount)
+        {
+            return new decimal();
+        }
+    }
+}
---
+++ ShoppingCar/ShoppingCarTest/DiscountCalcualtorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCar;
+
+namespace ShoppingCarTest
+{
+    [TestClass]
+    public class DiscountCalcualtorTest
+    {
+        [TestMethod]
+        public void Buy_series1_1_should_be_100()
+        {
+            var bookCount = 1;
+            var target= new DiscountCalcualtor();
+            var expected = 100;
+
+            var actual = target.CalcualtePayoutMoney(bookCount);
+
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
