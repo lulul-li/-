ecae0450fb3998759dd0361f701ff426f6981759
lulu <s987337613@gmail.com>
2017-06-19 23:06:48 +0800

    add buy series1-4 and series1-5 test case

--- ShoppingCar/ShoppingCarTest/DiscountCalcualtorTest.cs
+++ ShoppingCar/ShoppingCarTest/DiscountCalcualtorTest.cs
@@ -14,7 +14,7 @@ namespace ShoppingCarTest
         {
             var shoppingCar = new List<Book>()
             {
-                new Book { Series = 1 ,Count = 1}
+                new Book {Series = 1, Count = 1}
             };
             var target = new DiscountCalcualtor();
             var expected = 100;
@@ -29,7 +29,8 @@ namespace ShoppingCarTest
         {
             var shoppingCar = new List<Book>()
             {
-                new Book { Series = 1,Count = 1}, new Book { Series = 2,Count = 1 }
+                new Book {Series = 1, Count = 1},
+                new Book {Series = 2, Count = 1}
             };
             var target = new DiscountCalcualtor();
             var expected = 190;
@@ -44,10 +45,49 @@ namespace ShoppingCarTest
         {
             var shoppingCar = new List<Book>()
             {
-                new Book { Series = 1,Count = 1 }, new Book { Series = 2 ,Count = 1}, new Book { Series = 3 ,Count = 1}
+                new Book {Series = 1, Count = 1},
+                new Book {Series = 2, Count = 1},
+                new Book {Series = 3, Count = 1}
             };
             var target = new DiscountCalcualtor();
             var expected = 270;
+
+            var actual = target.CalculatePayoutMoney(shoppingCar);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_series1_1_series2_1_series3_1_series4_1_should_be_320()
+        {
+            var shoppingCar = new List<Book>()
+            {
+                new Book {Series = 1, Count = 1},
+                new Book {Series = 2, Count = 1},
+                new Book {Series = 3, Count = 1},
+                new Book {Series = 4, Count = 1}
+            };
+            var target = new DiscountCalcualtor();
+            var expected = 320;
+
+            var actual = target.CalculatePayoutMoney(shoppingCar);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_series1_1_series2_1_series3_1_series4_1_series5_1_should_be_375()
+        {
+            var shoppingCar = new List<Book>()
+            {
+                new Book {Series = 1, Count = 1},
+                new Book {Series = 2, Count = 1},
+                new Book {Series = 3, Count = 1},
+                new Book {Series = 4, Count = 1},
+                new Book {Series = 5, Count = 1}
+            };
+            var target = new DiscountCalcualtor();
+            var expected = 375;
 
             var actual = target.CalculatePayoutMoney(shoppingCar);
 
